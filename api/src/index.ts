93dfb814b47cbe2f4e5ee21f9baa682cf8920e07
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-17 14:42:26 +0530

    initial commit with backend folder structure

---
+++ api/src/index.ts
@@ -0,0 +1,9 @@
+import "dotenv/config"
+
+import app from './app';
+
+const port = process.env.PORT || 7000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
