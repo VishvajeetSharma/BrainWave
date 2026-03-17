a2401337c7503e3c2b2b7946423238431ad57645
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-17 15:26:47 +0530

    db connected

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,16 @@
 import "dotenv/config"
-
 import app from './app';
+import { AppDataSource } from "./dbconfig/dbconfig";
 
 const port = process.env.PORT || 7000;
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected successfully..");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
